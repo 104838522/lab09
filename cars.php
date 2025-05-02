2d9a9e47f4f9c7e005a95bec9998d46153145e03
Daehyeon Kim <104838522@student.swin.edu.au>
2025-05-03 00:29:09 +1000

    add search_form and search_result php files

--- cars.php
+++ cars.php
@@ -1,13 +1,12 @@
 <?php
     require_once 'settings.php';
-
-    if ($dbconn) {
+    if ($conn) {
         // Step 2: Create your SQL query
         $query = "SELECT * FROM cars";
     
         // Step 3: Execute your SQL query
-        $result = mysqli_query($dbconn, $query);
-        if ($result) {
+        $result = mysqli_query($conn, $query);
+        if ($result && mysqli_num_rows($result) > 0) {
             // Step 5: Show results in HTML table
             echo "<table border='1'>";
             echo "<tr><th>Car ID</th><th>Make</th><th>Model</th><th>Price</th><th>Year</th></tr>";
@@ -25,11 +24,11 @@
             echo "</table>";
             mysqli_free_result($result);
         } else {
-            echo "<p>Query failed.</p>";
+            echo "<p>There are no cars to display.</p>";
         }
 
         // Step 6: Close connection
-        mysqli_close($dbconn);
+        mysqli_close($conn);
 }else {
         echo "<p>Unable to connect to the database.</p>";
     }
--- settings.php
+++ settings.php
@@ -2,11 +2,11 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "s104838522_db"; // Replace with your actual DB name
+$database = "exhibition_db"; // Replace with your actual DB name
 
 $conn = mysqli_connect($host, $username, $password, $database);
 
-if (!$dbconn) {
+if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
